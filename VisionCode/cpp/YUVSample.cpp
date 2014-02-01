6d8573ae18e8fa1b7a6a7283ae099a40972c41e9
coconnell84 <coconnell84@gmail.com>
2014-02-01 16:23:07 -0500

    Final Hough Line transformation from start to finish

--- VisionCode/cpp/YUVSample.cpp
+++ VisionCode/cpp/YUVSample.cpp
@@ -4,55 +4,43 @@
 using namespace cv;
 using namespace std;
 
-Mat loadImage(char * pImageName)
-{
-	return imread(imageName, 1);
-}
-
 int main(int argc, char ** argv)
 {
 	char * imageName = argv[1];
 	cout<<"Image Name: "<<imageName<<endl<<" args: "<<argc<<endl;;
 
-	Mat image;
-	image = loadImage(imageName);
+	Mat image,yuvImage,houghImage;
+	Mat origImage;
+	image = imread(imageName,1);
+	origImage = imread(imageName,1);
 
 	if(!image.data)
 	{
 		cout<<"Error loading image: "<<imageName<<endl;
 		return 1;
 	}
-	namedWindow("ORIG_RGB", CV_WINDOW_AUTOSIZE);
-	imshow("ORG_RGB", image);
 
-	if(argc != 2 || !image.data)
-	{
-		printf("No image data \n");
-		return -1;
-	}
-	cvtColor(image,image,CV_BGR2YCrCb);
-	CvSize s = cvSize(image.rows, image.cols) ;
-	int d = image.depth();
+	//Convert to YUV color space. The goal of this 
+	//is to zero out the light channel (Y) 
+	cvtColor(image,yuvImage,CV_BGR2YCrCb);
+	CvSize s = cvSize(yuvImage.rows, yuvImage.cols) ;
+	int d = yuvImage.depth();
 
-
+	//Split he image into the Y, U and V channels
 	Mat planes[3]; 
-	//namedWindow("Orig_Image", CV_WINDOW_AUTOSIZE);
-	//imshow("Orig_Image", image);
-	split(image,planes);
+	split(yuvImage,planes);
 
+
+	//Zero out the Y channel
 	planes[0] = Mat::zeros(planes[0].rows,planes[0].cols,planes[0].depth());
-	merge(planes,3,image);
-
-	//namedWindow("PostImage", CV_WINDOW_AUTOSIZE);
-	//imshow("PostImage", image);
-
+	//Combine the channels back to the image
+	merge(planes,3,yuvImage);
 
 	//Convert back to RGB
-	cvtColor(image,image,CV_YCrCb2BGR);
-	//namedWindow("FinalImage", CV_WINDOW_AUTOSIZE);
-	//imshow("FinalImage", image);
-	imwrite("SimplyRed.jpg", image);
+	cvtColor(yuvImage,image,CV_YCrCb2BGR);
 
+	//Split the images to 0 out the blue and green channels. Will need to update
+	//to point the blue channel when looking for blue goal
 	split(image,planes);
 
 	planes[0] = Mat::zeros(planes[0].rows,planes[0].cols,planes[0].depth());
@@ -61,129 +49,44 @@ int main(int argc, char ** argv)
 
 	Mat src, dst, color_dst;
 	src = image;
-	Canny(src, dst, 50, 200,3);
+
+	//Canny edge detection. Will need to experiment with different threshold values:
+	//http://docs.opencv.org/doc/tutorials/imgproc/imgtrans/canny_detector/canny_detector.html
+
+	int canny_low_thresh = 50;
+	int canny_high_thresh = 200;
+	int canny_kernal_size = 3;
+	
+	Canny(src, dst, canny_low_thresh, canny_high_thresh,canny_kernal_size);
+
+	//Convert the image with canny edges back to BGR rep. 
 	cvtColor(dst, color_dst, CV_GRAY2BGR);
 	vector<Vec4i>lines;
-	HoughLinesP(dst, lines, 1, CV_PI/180, 80, 30, 10);
+
+
+	//Hough Lines
+	//http://docs.opencv.org/doc/tutorials/imgproc/imgtrans/hough_lines/hough_lines.html
+	double hough_rho = 1;
+	double hough_theta = CV_PI/180;
+	int hough_thresh = 80;
+	double hough_minLineLength = 30;
+	double hough_maxLineGap = 10;
+	HoughLinesP(dst, lines, hough_rho, hough_theta, hough_thresh, hough_minLineLength, hough_maxLineGap);
+
+
+	//Draw lines found by Hough transform
 	for(size_t i = 0; i<lines.size();i++)
 	{
 		line(color_dst, Point(lines[i][0], lines[i][1]),
 				Point(lines[i][2], lines[i][3]), Scalar(0,0,255),3,8);
 	}
-	vector<Vec3f>circles;
-	Mat gray;
-	cvtColor(src,gray,CV_BGR2GRAY);
-	HoughCircles(gray,circles, CV_HOUGH_GRADIENT,
-			gray.rows/32, //accumulator resolution
-			500,
-			100,
-			200,
-			50,
-			100);
+	//Show the original/unaltered image
+	namedWindow("ORIG_RGB", CV_WINDOW_AUTOSIZE);
+	imshow("ORIG_RGB", origImage);
 
-	cout<<"I found: " <<circles.size()<<" circles!"<<endl;
+        namedWindow("Final Image",CV_WINDOW_AUTOSIZE);
+	imshow("Final Image", color_dst);	
 
-	for(size_t i=0;i<circles.size();i++)
-	{
-		Point center(cvRound(circles[i][0]),cvRound(circles[i][1]));
-		int radius = cvRound(circles[i][2]);
-		circle(src,center,3,Scalar(0,255,0),-1,8,0);
-		circle(src,center,radius,Scalar(0,0,255),-3,8,0);
-	}
-	namedWindow("HOUGH_CIRCLE",CV_WINDOW_AUTOSIZE);
-	imshow("HOUGH_CIRCLE", src);
-	//namedWindow("Source", 1);
-	//	imshow("Source", src);
-
-	//namedWindow("Detected Lines",1);
-	//imshow("DetectedLines", color_dst);
-
-#if 0
-	//This is the simple, sample
-	Mat src, dst, color_dst;
-	src = imread(imageName, 0);
-	Canny(src, dst, 50, 200,3);
-	cvtColor(dst, color_dst, CV_GRAY2BGR);
-	vector<Vec4i>lines;
-	HoughLinesP(dst, lines, 1, CV_PI/180, 80, 30, 10);
-	for(size_t i = 0; i<lines.size();i++)
-	{
-		line(color_dst, Point(lines[i][0], lines[i][1]),
-				Point(lines[i][2], lines[i][3]), Scalar(0,0,255),3,8);
-	}
-	namedWindow("Source", 1);
-	imshow("Source", src);
-
-	namedWindow("Detected Lines",1);
-	imshow("DetectedLines", color_dst);
-
-	vector<Vec3f>circles;
-	HoughCircles(dst,circles, CV_HOUGH_GRADIENT,1,dst.rows/8,200,100,0,0);
-
-	for(size_t i=0;i<circles.size();i++)
-	{
-		Point center(cvRound(circles[i][0]),cvRound(circles[i][1]));
-		int radius = cvRound(circles[i][2]);
-		circle(dst,center,3,Scalar(0,255,0),-1,8,0);
-		circle(dst,center,radius,Scalar(0,0,255),-3,8,0);
-	}
-	namedWindow("HOUGH_CIRCLE",CV_WINDOW_AUTOSIZE);
-	imshow("HOUGH_CIRCLE");
-#endif	
-
-
-#if 0
-	//HoughLines(contours, lines, 1, CV_PI/180, 100, 0,0);
-
-	Mat cdst;
-	cvtColor(contours, cdst, CV_GRAY2BGR);
-
-	for(size_t i = 0; i<lines.size();i++)
-	{
-		float rho = lines[i][0], theta = lines[i][1];
-		Point pt1, pt2;
-		double a = cos(theta), b = sin(theta);
-		double x0 = a*rho, y0=b*rho;
-		pt1.x = cvRound(x0 + 20*(-b));
-		pt1.y = cvRound(y0 + 20*(a));
-		pt2.x = cvRound(x0 - 20*(-b));
-		pt2.y = cvRound(y0 - 20*(a));
-
-
-		line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
-	}
-	namedWindow( "detected_lines", CV_WINDOW_AUTOSIZE );
-	imshow( "detected_lines", cdst);
-#endif
-#if 0
-	Mat detected_edges = image;
-	//blur(image,detected_edges, Size(3,3));
-	Mat contours;
-	Canny(detected_edges, contours, 50,150); 
-
-	namedWindow("Canny");
-	imshow("Canny", contours);
-	vector<vector<Point> > contoursVector;
-	vector<Vec4i>hierarchy;
-	findContours(contours,contoursVector, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0,0)); 
-
-	Mat drawing = Mat ::zeros(contours.size(), CV_8UC3);
-
-	Vec4i largestContour;
-	double maxSize = 0;
-	vector<vector<Point> > contours_poly( contoursVector.size() );
-	for(int i=0;i<contoursVector.size();i++) 
-	{
-		//approxPolyDP(Mat(contoursVector[i]), contours_poly[i],3,true);
-		//Rect aBoundRect = boundingRect(Mat(contours_poly[i]));
-		Rect aBoundRect = boundingRect(Mat(contoursVector[i]));
-		Scalar color = Scalar(255,255,255 );
-		rectangle(drawing, aBoundRect.tl(), aBoundRect.br(), color, 2,8,0);
-	}
-	/// Show in a window
-	namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-	imshow( "Contours", drawing );
-#endif
 	waitKey(0);
 
 	return 0;
